05d018890414ec0309ed64db312df514ec88c5ec
Kozlovska Yuliia <kozlovskayayulya2@gmail.com>
2023-09-06 00:23:19 +0300

    userData

--- task1/src/Profile.jsx
+++ task1/src/Profile.jsx
@@ -3,7 +3,7 @@ import moment from 'moment';
 import './index.scss';
 import './profile.scss';
 
-const formatDate = (date) => moment(date).format('DD MMM YYYY');
+const formatDate = (date) => moment(date).format('DD MMM YY');
 
 const Profile = ({ userData }) => {
   const { firstName, lastName, birthDate, birthPlace } = userData;
